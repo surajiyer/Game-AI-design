373049c40fe7d9491bcebb71a2db5ebf70ab28d7
koen505 <koen.verhaegh@gmail.com>
2015-10-15 14:01:34 +0200

    Flags on terrain

--- core/src/mechanics/FlagList.java
+++ core/src/mechanics/FlagList.java
@@ -1,6 +1,7 @@
 package mechanics;
 
 import java.util.Random;
+import terrain.VoxelWorld;
 import utils.GameInfo;
 
 /**
@@ -12,49 +13,51 @@ public class FlagList {
     private Flag[] flagList;
     Random random = new Random();
     int[] allCoordinates;
+    VoxelWorld voxelworld;
 
-    public FlagList(int NROF_FLAGS) {
+    public FlagList(int NROF_FLAGS, VoxelWorld voxelworld) {
+        this.voxelworld = voxelworld;
         flagList = new Flag[NROF_FLAGS];
         allCoordinates = new int[NROF_FLAGS * 2];
         for (int i = 0; i < NROF_FLAGS; i++) {
             if (i == 0) {
                 int x = random.nextInt((GameInfo.widthField/3 - 20) + 1) + 20;
                 allCoordinates[i * 2] = x;
-                int y = 0;
                 int z = random.nextInt((GameInfo.heightField/2 - 20) + 1) + 20;
                 allCoordinates[i * 2 + 1] = z;
+                int y = (int) voxelworld.getHeight(x, z);
                 Flag flag = new Flag(x, y, z, i, 5);
                 flagList[i] = flag;
             } else if (i == 1) {
                 int x = random.nextInt((GameInfo.widthField/3 - 20) + 1) + 20;
                 allCoordinates[i * 2] = x;
-                int y = 0;
                 int z = random.nextInt((GameInfo.heightField-20 - GameInfo.heightField/2) + 1) + GameInfo.heightField/2;
                 allCoordinates[i * 2 + 1] = z;
+                int y = (int) voxelworld.getHeight(x, z);
                 Flag flag = new Flag(x, y, z, i, 5);
                 flagList[i] = flag;
             } else if (i == 2) {
                 int x = random.nextInt((GameInfo.widthField/3*2 - GameInfo.widthField/3) + 1) + GameInfo.widthField/3;
                 allCoordinates[i * 2] = x;
-                int y = 0;
                 int z = random.nextInt((GameInfo.heightField/4*3 - GameInfo.heightField/4) + 1) + GameInfo.heightField/4;
                 allCoordinates[i * 2 + 1] = z;
+                int y = (int) voxelworld.getHeight(x, z);
                 Flag flag = new Flag(x, y, z, i, 5);
                 flagList[i] = flag;
             } else if (i == 3) {
                 int x = random.nextInt((GameInfo.widthField-20 - GameInfo.widthField/3*2) + 1) + GameInfo.widthField/3*2;
                 allCoordinates[i * 2] = x;
-                int y = 0;
                 int z = random.nextInt((GameInfo.heightField/2 - 20) + 1) + 20;
                 allCoordinates[i * 2 + 1] = z;
+                int y = (int) voxelworld.getHeight(x, z);
                 Flag flag = new Flag(x, y, z, i, 5);
                 flagList[i] = flag;
             } else if(i==4) {
                 int x = random.nextInt((GameInfo.widthField-20 - GameInfo.widthField/3*2) + 1) + GameInfo.widthField/3*2;
                 allCoordinates[i * 2] = x;
-                int y = 0;
                 int z = random.nextInt((GameInfo.heightField-20 - GameInfo.heightField/2) + 1) + GameInfo.heightField/2;
                 allCoordinates[i * 2 + 1] = z;
+                int y = (int) voxelworld.getHeight(x, z);
                 Flag flag = new Flag(x, y, z, i, 5);
                 flagList[i] = flag;
             }
